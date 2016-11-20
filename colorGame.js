942fb4d532db71d5e052b8c25a01c9d814e05a09
WinnerSam <winnersam@gmail.com>
2016-11-20 18:23:03 +0200

    Fix 'Correct' bug

--- colorGame.js
+++ colorGame.js
@@ -47,6 +47,8 @@ resetButton.addEventListener("click", function(){
 	pickedColor = pickColor();
 	//Change colorDisplay to match picked color
 	colorDisplay.textContent = pickedColor;
+	
+messageDisplay.textContent = "";
 	//Change colors of squares
 	for(var i = 0; i < squares.length; i++){
 		squares[i].style.background = colors[i];
